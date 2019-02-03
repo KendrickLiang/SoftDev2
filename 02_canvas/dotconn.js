91c7779c60557a26adacb30d6dea41e7ae4903d5
KendrickLiang <kliang5@stuy.edu>
2019-02-02 23:31:42 -0500

    stuff

--- 02_canvas/dotconn.js
+++ 02_canvas/dotconn.js
@@ -1,11 +1,11 @@
 /* 
 Can'tvas - Kendrick Liang, Ricky Lin 
 SoftDev2 pd06
-K #01: ...and I want to Paint It Better
-F 2019-02-01
+K #02: Connecting the Dots
+M 2019-02-04
 */ 
 
-console.log("Entering destruction mode")
+console.log("Entering destruction mode");
 
 var canvas = document.getElementById("playground");
 
@@ -13,18 +13,20 @@ if (canvas.getContext) {
     var ctx = canvas.getContext("2d");
 }
 
-var clear = document.getElementById("clear")
+var clear = document.getElementById("clear");
+var numDots = 0;
 
 clear.addEventListener('click', function() {
     console.log("Exterminating everything in sight");
     ctx.clearRect(0, 0, canvas.clientWidth, canvas.clientHeight);  //clearing entire width and height of canvas
+    numDots = 0;
 });
 
 
-canvas.addEventListener('click', draw)
+canvas.addEventListener('click', draw);
 
-var oldX = -1;
-var oldY = -1;
+var oldX;
+var oldY;
 
 function draw(event) {
 
@@ -37,16 +39,16 @@ function draw(event) {
     var y = event.offsetY;  //offset y provides the y coordinate of the event (mouse click) relative to the top edge of target element (the canvas)
                             //They are used to make sure that the shapes were drawn right where the mouse was in the canvas.
     
-    if (oldX < 0 && oldY < 0) {
-        oldX = x;
-	oldY = y;
-    }
-    else {
+    oldX = x;
+    oldY = y;
+    numDots += 1;
+    if (numDots >= 2) {
         ctx.lineTo(oldX, oldY);
+	ctx.stroke();
     }
     ctx.beginPath(); //beginpath ends the current drawing and begins a new one. 
                          //We used this to initiate the drawing of a new shape, especially if the user switched from box to dot.
-    ctx.fillStyle = '#' + Math.random().toString(16).substring(4,10)
+    ctx.fillStyle = '#' + Math.random().toString(16).substring(4,10);
     ctx.ellipse(x, y, 10, 10, Math.PI * .25, 0, Math.PI * 2);  //create dot at center of mouse, 10x10, rotating by pi/4 from 0 to 2pi) 
     ctx.stroke();
     ctx.fill();
